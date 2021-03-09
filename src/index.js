48f1ae3a79ab5887665fd6cb00c2dc0aede3e4fc
Pavel <naxa@nextmail.ru>
2021-03-10 00:29:16 +0100

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    const isDigit = c => '0' <= c && c <= '9';
+    return +n.toString()
+        .split('')
+        .filter(isDigit)
+        .reverse()
+        .join('');
 }
